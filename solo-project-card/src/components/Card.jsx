864599fdb1bb0b97dc5979a0ce791fcf88c8b8ec
aimee-annabelle <inezannabelle@gmail.com>
2024-07-19 12:41:25 +0200

    sticky footer on bottom

--- solo-project-card/src/components/Card.jsx
+++ solo-project-card/src/components/Card.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 export default function Card() {
   return (
     <div className="w-2/5 h-[100vh] py-11 px-28 bg-zinc-800 flex justify-center">
-    <div className="h-full w-9/12 bg-[#1a1b21] rounded-xl text-white text-center">
+    <div className="relative h-full w-9/12 bg-[#1a1b21] rounded-xl text-white text-center">
       <img
         src={image}
         alt=""
@@ -19,7 +19,7 @@ export default function Card() {
         </button>
       </div>
       <About />
-      <div className="bg-neutral-900 w-full rounded-b-xl px-8">
+      <div className="bg-neutral-900 w-full absolute bottom-0 rounded-b-xl px-8">
         <Footer />
       </div>
     </div>
