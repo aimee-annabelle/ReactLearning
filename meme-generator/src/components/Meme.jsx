7252451060864e68e79896753045c8a0097a766f
aimee-annabelle <inezannabelle@gmail.com>
2024-07-23 12:05:58 +0200

    retrieve images with state

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,11 +1,41 @@
-export default function Meme(){
-    return(
+import { useState } from "react";
+import memeImages from "../memeData.js";
+export default function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+  const getImage = () => {
+    const memeArray = memeImages.data.memes;
+    const randomImage = memeArray[Math.floor(Math.random() * memeArray.length)];
+    setMemeImage(randomImage.url);
+  };
+  return (
+    <div className="p-5 flex flex-col justify-center">
+      <div className="p-5 gap-5 grid grid-cols-2 grid-rows-2">
         <div>
-            <form action=""className="p-5 gap-5 grid grid-cols-2 grid-rows-2">
-                <input type="text" placeholder="Top Text" className="w-full p-3 rounded-lg border indent-3"/>
-                <input type="text" placeholder="Bottom Text" className="w-full p-3 rounded-lg border indent-3"/>
-                <button type="submit" className="w-full p-3 rounded-lg text-white font-bold text-lg col-span-2 border bg-customGradient">Get a new meme image 🖼</button>
-            </form>
+          <label htmlFor="topText">Top Text</label>
+          <input
+            id="topText"
+            type="text"
+            placeholder="Top Text"
+            className="w-full p-3 rounded-lg border indent-3"
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="bottomText">Bottom text</label>
+          <input
+            id="bottomText"
+            type="text"
+            placeholder="Bottom Text"
+            className="w-full p-3 rounded-lg border indent-3"
+          />
+        </div>
+        <button
+          className="w-full p-3 rounded-lg text-white font-bold text-lg col-span-2 border bg-customGradient"
+          onClick={getImage}
+        >
+          Get a new meme image 🖼
+        </button>
+      </div>
+      {memeImage && <img src={memeImage} className="p-5 w-auto h-auto" />}
+    </div>
+  );
 }
--- meme-generator/src/components/Navbar.jsx
+++ meme-generator/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import logo from '../../public/logo.svg'
+import logo from "/logo.svg";
 export default function Navbar() {
   return (
     <div className="w-full bg-customGradient h-20 flex items-center justify-between px-10 text-white">
-      <div className='flex gap-2'>
-      <img src={logo} alt="" />
-      <h1 className="font-bold text-xl">Meme Generator</h1>
+      <div className="flex gap-2">
+        <img src={logo} alt="" />
+        <h1 className="font-bold text-xl">Meme Generator</h1>
       </div>
-      
+
       <p>React Course - Project 3</p>
     </div>
   );
